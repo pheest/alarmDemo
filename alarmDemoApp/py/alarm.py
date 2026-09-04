11ec1b5d17ac9cd4f7619fcb17817bb4a9543ba5
Peter Heesterman <pheest@gmail.com>
2026-09-04 15:30:32 +0100

    Added timer as replacement of existing OUT_PINI:NO record. Most records now process successfully without needing to specify VAL.

--- alarmDemoApp/py/alarm.py
+++ alarmDemoApp/py/alarm.py
@@ -1,14 +1,15 @@
 import devsup.ptable as PT
 from devsup.ptable import _ONPROC
 from devsup.hooks import addHook
-from devsup import NO_ALARM, MINOR_ALARM, MAJOR_ALARM, INVALID_ALARM, COMM_ALARM, READ_ALARM, WRITE_ALARM
+from devsup import NO_ALARM, MINOR_ALARM, MAJOR_ALARM, INVALID_ALARM, READ_ALARM, WRITE_ALARM
 from devsup.db import getRecord
 import signal
 from threading import Thread, Event
 
 
 class AlarmTest(PT.TableBase):
-    # Out paramaneters
+    # Output paramaters
+    timer = PT.Parameter()
     out_pini_no = PT.Parameter()
     out_pini_yes = PT.Parameter()
     # Input parameters
@@ -20,6 +21,7 @@ class AlarmTest(PT.TableBase):
         self.stop = False
         self.count = None
         self.onproc_event = Event()
+        self.timer.addAction(self.proc_out_timer, _ONPROC)
         self.out_pini_no.addAction(self.proc_out_pini_no, _ONPROC)
         self.out_pini_yes.addAction(self.proc_out_pini_yes, _ONPROC)
         self.run_thread = Thread(target=self._run)
@@ -39,20 +41,26 @@ class AlarmTest(PT.TableBase):
         These two PVs would be either the inputs and driven from the _run thread,
         or output PVs and driven from the onproc method(s).
         """
-        if self.count > 10:
-            PV1.stat = READ_ALARM
-            PV2.stat = WRITE_ALARM
-            
-        if self.count % 2:
-            PV1.alarm = MAJOR_ALARM
-            PV1.amsg = "odd number"
-            PV2.alarm = NO_ALARM
-            PV2.amsg = None
-        else:
-            PV1.alarm = NO_ALARM
-            PV1.amsg = None
-            PV2.alarm = MAJOR_ALARM
-            PV2.amsg = "even number"
+        if PV1 is not None:
+            if PV1.value % 2:
+                PV1.alarm = MAJOR_ALARM
+                PV1.amsg = "odd number"
+                if PV1.value > 10:
+                    # Just to show that other alarm status value than COMM are possible.
+                    PV1.stat = READ_ALARM
+            else:
+                PV1.alarm = NO_ALARM
+                PV1.amsg = None
+        if PV2 is not None:
+            if PV2.value % 2:
+                PV2.alarm = NO_ALARM
+                PV2.amsg = None
+            else:
+                PV2.alarm = MAJOR_ALARM
+                PV2.amsg = "even number"
+                if PV2.value > 10:
+                    # Just to show that other alarm status value than COMM are possible.
+                    PV2.stat = WRITE_ALARM
         
     def _run(self):
         while True:
@@ -70,24 +78,33 @@ class AlarmTest(PT.TableBase):
             print("in_pini_no", self.in_pini_no.value)
             print("in_pini_yes", self.in_pini_yes.value)
 
-    def proc_out_pini_no(self):
+    def proc_out_timer(self):
         """
         This method is called in response to timer scanning within the database scan.
         This drives the application timing.
         """
+        if self.count is None:
+            if len(self.out_pini_yes.value) > 0:
+                self.count = self.out_pini_yes.value[0]
         if self.count is not None:
             self.onproc_event.set()
-
+        
             out_pini_no = getRecord("OUT_PINI:NO")
-            # This cannot work here to update alarm status.
-            # If this call scanned the (own) record it would cause an infinite number of calls.
             out_pini_no.VAL = self.count
+            # Scan not required here because it is a scalar output record.
+            # out_pini_no.scan(sync=False, force=0)
+
             out_pini_yes = getRecord("OUT_PINI:YES")
-            # I believe this only works correctly for waveform records.
-            # The record is scanned to update the scan status.
             out_pini_yes.VAL = [self.count]
+            # The record needs to be scanned to update the scan status.
             out_pini_yes.scan(sync=False, force=0)
             self.count += 1
+
+    def proc_out_pini_no(self):
+        if self.count is not None:
+            self.set_alarms(self.out_pini_no, None)
+            print("out_pini_no", self.out_pini_no.value)
+
         
     def proc_out_pini_yes(self):
         if self.count is None:
@@ -96,7 +113,7 @@ class AlarmTest(PT.TableBase):
         if self.count is not None:
             # This is in setting the alarm status on what is an output record.
             # This is - in effect - asyn:readback.
-            self.set_alarms(self.out_pini_no, self.out_pini_yes)
+            self.set_alarms(None, self.out_pini_yes)
             print("out_pini_yes", self.out_pini_yes.value)
 
 
